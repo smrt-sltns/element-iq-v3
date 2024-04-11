3a33f1ce2c0c59e8620e58941f2b002f4b00697a
coboaccess <coboaccess@gmail.com>
2024-04-11 11:25:31 -0400

    image apis are working with tokens

--- image_apis/views.py
+++ image_apis/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from account.models import User
 
 import json
 
+
+@csrf_exempt
 def decrease_tokens(request):
-    data = json.loads(request.body)
+    if request.method != "POST":
+        return JsonResponse({"error": "method not allowed"})
+    user_id = request.POST.get("userId")
+    token_used = request.POST.get("amount")
     try:
-        user = User.objects.get(id=data["userId"])
-        user.token -= data["amount"]
+        user = User.objects.get(id=user_id)
+        user.token -= int(token_used)
         user.save()
     except User.DoesNotExist as e:
         return JsonResponse({"status": "error", "error": e.__str__})
 
-    return JsonResponse({"status": "ok"})
+    return JsonResponse({"status": "ok", "remainingTokens": user.token})
 
 
 @login_required
